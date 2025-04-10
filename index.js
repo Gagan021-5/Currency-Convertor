31ef6fb9f30c312a719d2be26f382c6043d0af9a
Gagan021-5 <gagansingh010221@gmail.com>
2025-04-10 18:14:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,233 @@
+const  a = document.querySelectorAll(".content select");
+const b = document.querySelector(".my");
+const btn = document.querySelector(".btn");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const dis = document.querySelector(".final h2");
+const audio = new Audio("cash.mp3");
+console.log(audio);
+
+
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+  };
+
+  const flag=(e)=>{
+    let first = e.value;
+    let sec = countryList[first];
+    let link  = `https://flagsapi.com/${sec}/flat/64.png`;
+    let update = e.parentElement.querySelector("img");
+    update.src = link;
+  }
+  
+for(let val of a){
+  for(let curr in countryList){
+    let neww = document.createElement("option");
+      neww.value =curr;
+      neww.innerText = curr;
+      val.append(neww);
+      neww.style.backgroundColor ="#004d7a";
+
+      if(val.name ==="first" && curr ==="CNY"){
+        neww.selected = "selected";
+      } else if(val.name ==="second" && curr ==="INR"){
+        neww.selected = "selected";
+      } 
+  }
+ val.addEventListener("change",(e)=>{
+     flag(e.target);
+ })
+  
+}
+
+
+btn.addEventListener("click",async (e)=>{
+  e.preventDefault();
+ 
+  let amount = b.value;
+  if(amount ==="" || amount<0){
+   alert("Please Enter a Valid Amount!");
+   b.value = 0;
+  }
+
+ console.log(one.value,two.value);
+ 
+  const url = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${one.value.toLowerCase()}.json`);
+ console.log(url);
+
+  let data =  await url.json();
+   let rate = data[one.value.toLowerCase()][two.value.toLowerCase()];
+   console.log(rate);
+   let result = amount * rate;
+
+  if(amount!=0){
+     dis.innerText = `${amount} ${one.value} = ${result} ${two.value}`;
+     audio.play();
+  }
+   
+   
+
+});
+
+
+
+
+
